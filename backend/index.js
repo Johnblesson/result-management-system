f6aa8a1071da63eb8fd0b5312b19d749d51a95cd
John Blesson-Rowe <106766947+Johnblesson@users.noreply.github.com>
2023-09-26 09:43:05 +0000

    update

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+require("dotenv").config()
+
+// Database Connection
+const connectDB = require('./database/connection')
+connectDB;
+
+// Middleware
+app.use(bodyParser.json())
+app.use(cors())
+
+// Linking the studentRoute and the Admin Credentials (login and signup)
+const studentsRouter = require('./routes/students')
+const adminLoginSignup = require('./routes/adminRoute')
+
+app.use('/api/v1/student', studentsRouter)
+app.use('/api/v1/admin', adminLoginSignup)
+
+// Starting server
+const PORT = process.env.PORT || 8080
+app.listen(PORT, (error) => {
+    if (error) {
+        console.log('Message: Something went wrong')
+    } else {
+        console.log(`Server running on port ${PORT}`)
+    }
+})
